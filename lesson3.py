682b85a08fdda8f27a807d93c0cd07d19cd58f15
Leynard Drake Hernandez <leynarddrakehernandez@gmail.com>
2025-09-17 02:49:34 +0800

    lesson 3

---
+++ lesson3.py
@@ -0,0 +1,60 @@
+# LESSON 3 INTERMEDIATE PYTHON
+
+# Conditions
+
+# x = 2
+# comparasison 
+#  is use to compare values, returns boolean value
+# == ,  !=,  < >, >= <=, 
+# print (x == 2)
+# print (x > 2)
+
+
+# x = [1,2,3]
+# y = [1,2,3]
+# # prints true because equal in value
+# print (x == y)
+# # prints print false bc not same memory address will print true it x = y
+# print (x is y)
+
+
+# LOOPS
+
+# for loop
+# primes =  [2,3,5,7]
+# for prime in primes:
+#     print (prime)
+
+# prints 0 to 4
+# for x in range (5):
+#     print (x)
+
+# prints 3 to 9 doesnt include 10
+# for x in range(3, 10):
+#     print (x)
+
+# loop till 9 and last parameter is how much it will add every loop
+# for x in range(3, 10,2):
+#     print (x)
+
+# while loops
+# count = 5
+# while count > 0:
+#     print (count)
+#     count -= 1
+
+
+# When the loop condition of "for" or "while" statement fails then code part
+# in "else" is executed. If a break statement is executed inside the for loop then the "else" part is skipped. Note that the "else" part is
+# executed even if there is a continue statement.
+
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+# else:
+#     print ("count value reached 5")
+
+# for i in range ()
+    
