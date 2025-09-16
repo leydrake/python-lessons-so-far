2d9a9a5b40edf4762832f49203d41a9746332482
Leynard Drake Hernandez <leynarddrakehernandez@gmail.com>
2025-09-16 17:37:23 +0800

    reviewer for lesson 2

---
+++ lesson2.py
@@ -0,0 +1,48 @@
+list = [1,2,3]
+
+# x,y,z = list
+
+# for i in list:
+#     print(i)
+
+print ("=" * 30)
+
+# append is adding value to the last index
+# list.append(1)
+print ("=" * 30)
+
+# arithmetic / order of precedence 
+# number = 1 + 2 * 3/ 4.0
+# print(number)
+
+# squared = 7**3
+# cubed = 2 ** 3
+
+# print (squared)
+# print (cubed)
+
+print ("=" * 30)
+
+# STRING OPERATORS
+astring = "Hello world!"
+
+
+# index of returns the index where the value is first found
+# print (astring.index("o"))
+
+# rfind is lastIndexOf returns the index where the value is last found
+# print (astring.rfind("o"))
+
+# returns the value of index 3 to 6... doesnt include the last which is index 7
+# print(astring [3:7])
+# output = lo w
+
+#  turns the text to upper or lower case
+#  print(astring.upper())
+# print(astring.lower())
+
+
+# checks the value of the string if it matches the value in parameter
+# returns boolean value (true or false)
+# print(astring.startswith("Hello"))
+# print(astring.endswith("asdfasdfasdf"))
